1f0529ef31f9217d610e15ad2dffc4d941d53eb6
devasindim <89437640+devasindim@users.noreply.github.com>
2023-05-31 15:26:02 +0700

    Create beranda.php

---
+++ SPK/Page/beranda.php
@@ -0,0 +1,21 @@
+<!-- judul -->
+<div class="panel">
+    <!-- <div class="panel-middle" id="judul">
+        <img src="asset/image/alternatif.svg" style: >
+        <div id="judul-text">
+            <h2 class="text-pink">BERANDA</h2>
+            <p>Halaman Utama Sistem Pendukung Keputusan</p>
+        </div>
+    </div>
+</div> -->
+<!-- judul -->
+<div class="panel">
+    <div class="panel-middle text-center">
+        <h1>
+            Halo! Selamat Datang di <br>
+            <span class="text-pink">SPK Pemilihan Laptop Berbasis WEB</span><br>
+            Menggunakan Metode<br>Simple Additive Weighting (SAW),<br> Weighted Product (WP), dan Profile Matching (PM)
+        </h1>
+    </div>
+    <div class="panel-bottom"></div>
+</div>
