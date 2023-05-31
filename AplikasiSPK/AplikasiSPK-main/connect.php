e3f3fdf7de7a72d5943917af1962aa17c0ea4d52
devasindim <89437640+devasindim@users.noreply.github.com>
2023-05-31 15:28:17 +0700

    Delete AplikasiSPK/AplikasiSPK-main directory

--- AplikasiSPK/AplikasiSPK-main/connect.php
+++
@@ -1,6 +0,0 @@
-<?php
-$konek=new mysqli('localhost','root','','projectspk');
-if ($konek->connect_errno){
-    "Database Error".$konek->connect_error;
-}
-?>
--- AplikasiSPK/AplikasiSPK-main/index.php
+++
@@ -1,7 +0,0 @@
-<?php
-session_start();
-if (empty($_SESSION['user']) AND empty($_SESSION['pass'])){
-    include 'login.php';
-}else{
-    include 'admin.php';
-}
