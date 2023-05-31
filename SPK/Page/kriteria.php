ccc91804171e5d10c08fbb448a0c83ee26188db1
devasindim <89437640+devasindim@users.noreply.github.com>
2023-05-31 15:26:40 +0700

    Create kriteria.php

---
+++ SPK/Page/kriteria.php
@@ -0,0 +1,70 @@
+<!-- judul -->
+<div class="panel">
+    <div class="panel-middle" id="judul">
+        <img src="asset/image/kriteria.svg">
+        <div id="judul-text">
+            <h2 class="text-pink">KRITERIA</h2>
+            Halamanan Sistem Informasi Kriteria
+        </div>
+    </div>
+    </div>
+    <!-- judul -->
+    <div class="panel">
+    <div class="panel-middle text-center">
+                <b class="text-pink">Data Kriteria</b>
+            <!-- </div>
+            <div class="panel-middle"> -->
+                <div class="table-responsive">
+                    <table>
+                    <style>
+                    .warna-biru {
+                           background-color: #a3d5ff;
+                           color: white;
+                    }
+                    </style>
+                    <table>
+                    <thead>
+                                    <tr class="warna-biru">
+                                    <th>No</th>
+                                    <th>Kode Kriteria</th>
+                                    <th>Kategori Kriteria</th>
+                                    <th>Jenis</th>
+                                    </tr>
+                    </thead>
+                            <tbody>
+                                    <td>1</td>
+                                    <td>C1</td>
+                                    <td>Processor</td>
+                                    <td>Benefit</td>
+                                    <div class='norebuttom'>
+                                </div></tr>
+                                    <td>2</td>
+                                    <td>C2</td>
+                                    <td>RAM</td>
+                                    <td>Benefit</td>
+                                    <div class='norebuttom'>
+                                </div></tr>
+                                    <td>3</td>
+                                    <td>C3</td>
+                                    <td>Harddisk</td>
+                                    <td>Benefit</td>
+                                    <div class='norebuttom'>
+                                </div></tr>
+                                    <td>4</td>
+                                    <td>C4</td>
+                                    <td>VGA</td>
+                                    <td>Benefit</td>
+                                    <div class='norebuttom'>
+                                </div></tr>
+                                    <td>5</td>
+                                    <td>C5</td>
+                                    <td>Harga</td>
+                                    <td>Cost</td>
+                                    <div class='norebuttom'>
+                                </div></tr>                        </tbody>
+                    </table>
+                    </div>
+            </div>
+            <div class="panel-bottom"></div>
+        </div>
+    </div>
