63a63d2a4c0da3acf814c71f5da3134543669ec5
devasindim <89437640+devasindim@users.noreply.github.com>
2023-05-31 15:27:01 +0700

    Create metode.php

---
+++ SPK/Page/metode.php
@@ -0,0 +1,53 @@
+<!-- judul -->
+<div class="panel">
+    <div class="panel-middle" id="judul">
+        <img src="asset/image/metode.svg">
+        <div id="judul-text">
+            <h2 class="text-pink">METODE</h2>
+            Halamanan Sistem Informasi Metode
+        </div>
+    </div>
+    </div>
+    <!-- judul -->
+    <div class="panel">
+    <div class="panel-middle text-center">
+                <b class="text-pink">Data Metode</b>
+            <!-- </div> -->
+            <div class="panel-middle">
+                <div class="table-responsive">
+           <style>
+           .warna-biru {
+                  background-color: #a3d5ff;
+                  color: white;
+           }
+           </style>
+           <table>
+           <thead>
+                 <tr class="warna-biru">
+                 <th>No</th>
+                 <th>Jenis Metode</th>
+                 </tr>
+           </thead>
+           <tbody>
+                 <tr>
+                 <td>1</td>
+                 <td>Simple Additive Weighting (SAW)</td>
+                 <div class='norebuttom'></div>
+                 </tr>
+                 <tr>
+                 <td>2</td>
+                 <td>Weighted Product (WP)</td>
+                 <div class='norebuttom'></div>
+                 </tr>
+                 <tr>
+                 <td>3</td>
+                 <td>Profile Matching (PM)</td>
+                 <div class='norebuttom'></div>
+                 </tr>
+           </tbody>
+           </table>
+                </div>
+           </div>
+           <div class="panel-bottom"></div>
+        </div>
+    </div>
